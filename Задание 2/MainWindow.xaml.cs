467f0a54be08318b43dce95a9ae74a9ec4b64e8b
RuslanVasiykov4567 <ryslanvaciykov@gmail.com>
2023-12-16 12:31:09 +0900

    first commit

---
+++ Задание 2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(text1.Text);
+            int b = Convert.ToInt32(text2.Text);
+            int sum = a + b;
+            label1.Content = sum;
+        }
+
+        private void text1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
